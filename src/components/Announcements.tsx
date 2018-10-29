a24d116acab520a8e4ac1c8e9beee12a38a1759a
Matthew Leon <matthew.leon@vanderbilt.edu>
2018-10-28 22:51:56 -0500

    Ft. forced compositing and a few tiny layout tweaks

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -44,7 +44,7 @@ export class Announcements extends React.Component<{}, AnnouncementState> {
                     });
                 }
             }
-        }
+        };
     }
 
     render() {
