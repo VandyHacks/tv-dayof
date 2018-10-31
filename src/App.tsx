2ad1fdcad387427c1820d33129e549d8be39a2f1
Matthew Leon <matthew.leon@vanderbilt.edu>
2018-10-30 20:20:10 -0500

    Schedule auto-updates as the event goes on

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Header from './components/Header';
 import { Announcements } from './components/Announcements';
-import { Schedule } from './components/Schedule';
+import { Schedule, sortEvents } from './components/Schedule';
+import { events, EventRecord } from '../assets/schedule';
+import { List } from 'immutable';
 import '../assets/scss/App.scss'
 
-class App extends React.Component<{}, {}> {
+class App extends React.Component<{}, { events: List<EventRecord> }> {
+    componentWillMount() {
+        this.setState({
+            events: sortEvents(events),
+        });
+    }
+
     render() {
         return <div className='Main'>
             <Header />
             <Announcements />
-            <Schedule />
+            <Schedule allEvents={this.state.events} />
         </div>
     }
 }
