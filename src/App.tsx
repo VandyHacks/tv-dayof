6ce4f7219d655420d12037faeb10930856cd0ae3
Matthew Leon <matthew.leon@vanderbilt.edu>
2018-10-31 16:59:53 -0500

    Update time

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ class App extends React.Component<{}, { events: List<EventRecord> }> {
 
     render() {
         return <div className='Main'>
-            <Header countdownDate={new Date(2018, 10, 2, 5)} />
+            <Header countdownDate={new Date(2018, 10, 2, 18)} />
             <Announcements />
             <Schedule allEvents={this.state.events} />
         </div>
